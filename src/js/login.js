5a028deff501926f1e7e7b6889d6be13fa8fd801
Ngo Van Thuc <ngothuc0704@gmail.com>
2024-05-30 23:04:47 +0700

    ngothuc ne

--- src/js/login.js
+++ src/js/login.js
@@ -2,3 +2,8 @@ const handleSubmit = (event) => {
     event.preventDefault();           
     window.location.href = "../view/main.html";
 }
+
+const handleLogin = (event) => {
+    event.preventDefault();           
+    window.location.href = "../view/main.html";
+}
